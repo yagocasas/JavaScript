3e1dcab6a3884460f6fa809274745c84879fe707
yagocasas <yagocd10@hotmail.com>
2022-10-03 21:18:29 +0200

    first session

---
+++ js_clase_1/index.js
@@ -0,0 +1,22 @@
+// console.log('Esto es una prueba de impresión'); 
+
+// const testDiv = document.getElementById('test');
+// console.log('DIV CAPTURADO', testDiv);
+
+// alert('Hola, esto es un alert');
+
+// console.log(process);
+
+// console.log('Hola');
+// console.log('Adios');
+
+// var pepe1 = 'Pepe1';
+// var $pepe1 = 'Pepe2';
+// var _pepe = 'Pepe3';
+
+// console.log(type of pepe1);
+
+var saludo = prompt('Cómo quiere saludar?');
+
+console.log('El usuario quiere saludar:');
+
