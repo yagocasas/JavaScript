06981ea52e8d80a907b67cf0ccc9c87913e5b78c
yagocasas <yagocd10@hotmail.com>
2022-10-04 01:46:54 +0200

    Iteration#3

---
+++ upgrade-basic-algorithms/operadores.js
@@ -0,0 +1,30 @@
+ 
+ 
+ // 1.1 Multiplica 10 por 5 y muestra el resultado mediante alert. 
+ 
+alert(10 * 5);  
+ 
+  
+ // 1.2 Divide 10 por 2 y muestra el resultado en un alert. 
+
+alert(10 / 2);
+
+
+// 1.3 Muestra mediante un alert el resto de dividir 15 por 9. 
+
+alert(15 % 9);
+
+
+ // 1.4 Usa el correcto operador de asignación que resultará en x = 15, 
+ // teniendo dos variables y = 10 y z = 5. 
+
+ var y = 10;
+ var z = 5;
+ var x = y + z;
+ console.log(x);
+
+// 1.5 Usa el correcto operador de asignación que resultará en x = 50,
+ // teniendo dos variables y = 10 y z = 5.   
+
+ x = y * z;
+ console.log(x);
