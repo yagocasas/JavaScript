8b9525c1ecf3c430f397c1d7684d728b178b9802
yagocasas <yagocd10@hotmail.com>
2022-10-04 12:47:50 +0200

    Iteration#6

--- upgrade-basic-algorithms/bucles.js
+++ upgrade-basic-algorithms/bucles.js
@@ -1,12 +1,12 @@
 // 1.1 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola.
-for (let i = 0; i <= 9; i++);
-console.log('bucle 1');
+for (var i = 0; i <= 9; i++);
+console.log(i);
 
 // 1.2 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola solo 
 // cuando el resto del numero dividido entre 2 sea 0.
-for (let i = 0; i <= 9; i++) {
-    if (i % 2 === 0) {
-        console.log('bucle 2');
+for (let j = 0; j <= 9; j++) {
+    if (j % 2 === 0) {
+        console.log(j);
     }
 } 
 
@@ -14,10 +14,10 @@ for (let i = 0; i <= 9; i++) {
 // Este bucle empieza en 0 y termina en 10. 
 // Muestra por consola un mensaje diciendo 'Intentando dormir' en cada vuelta del bucle 
 // y cambia el mensaje en la última vuelta a 'Dormido!'.
-for (let i = 0; i <= 10; i++) {
-    if (i <= 9) {
+for (let h = 0; h <= 10; h++) {
+    if (h <= 9) {
         console.log('Intentando dormir')
-    } if (i ==10) {
+    } if (h ==10) {
         console.log('Dormido!')
     }
 }
