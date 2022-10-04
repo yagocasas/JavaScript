d7d3b3b357225b9cd1584f341cf0068e34585c51
yagocasas <yagocd10@hotmail.com>
2022-10-04 23:33:57 +0200

    Iteration#1

---
+++ js-functions/functions1.js
@@ -0,0 +1,7 @@
+// **Iteración #1: Buscar el máximo**
+
+// Completa la función que tomando dos números como argumento devuelva el más alto.
+
+function sum(numberOne , numberTwo) {
+    return Math.max(numberOne, numberTwo)
+  }
