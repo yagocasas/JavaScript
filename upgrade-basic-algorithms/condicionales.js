8eb2bb1ac4a499bef08f021f65e826530b65f2b5
yagocasas <yagocd10@hotmail.com>
2022-10-04 14:35:28 +0200

    CHANGE condicionales

--- upgrade-basic-algorithms/condicionales.js
+++ upgrade-basic-algorithms/condicionales.js
@@ -23,7 +23,7 @@ if (number3 * 5 == number1) {
   console.log("number3 por 5 es igual a number1");
 }
 
-if (number3 * 5 == number1 & number1 * 2 == number2) {
+if (number3 * 5 == number1 && number1 * 2 == number2) {
   console.log("number3 por 5 es igual a number1 Y number1 por 2 es igual a number2");
 }
 
---
+++ upgrade-basic-algorithms/deporvida.js
@@ -0,0 +1,8 @@
+let nBags = 1000;
+let nTotalDays = 1000 / 1.5;
+let nTotalMonths = nTotalDays / 30;
+let nTotalYears = nTotalMonths / 12;
+
+console.log(nTotalDays + ' días.');
+console.log(nTotalMonths + ' meses.');
+console.log(nTotalYears + ' años');
