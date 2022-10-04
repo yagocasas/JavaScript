56633587bb75b397528d56cf3c81b08e7c347c9e
yagocasas <yagocd10@hotmail.com>
2022-10-04 09:03:38 +0200

    ADD console.log(alert)

--- upgrade-basic-algorithms/operadores.js
+++ upgrade-basic-algorithms/operadores.js
@@ -3,17 +3,18 @@
  // 1.1 Multiplica 10 por 5 y muestra el resultado mediante alert. 
  
 alert(10 * 5);  
+console.log(alert)
  
   
  // 1.2 Divide 10 por 2 y muestra el resultado en un alert. 
 
 alert(10 / 2);
-
+console.log(alert)
 
 // 1.3 Muestra mediante un alert el resto de dividir 15 por 9. 
 
 alert(15 % 9);
-
+console.log(alert)
 
  // 1.4 Usa el correcto operador de asignación que resultará en x = 15, 
  // teniendo dos variables y = 10 y z = 5. 
