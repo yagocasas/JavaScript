85166ec6d08ff35c61e32aab6437a465ec78d464
yagocasas <yagocd10@hotmail.com>
2022-10-04 11:12:08 +0200

    Iteration#4

--- upgrade-basic-algorithms/operadores.js
+++ upgrade-basic-algorithms/operadores.js
@@ -1,20 +1,14 @@
- 
- 
- // 1.1 Multiplica 10 por 5 y muestra el resultado mediante alert. 
- 
-alert(10 * 5);  
-console.log(alert)
- 
-  
- // 1.2 Divide 10 por 2 y muestra el resultado en un alert. 
+// 1.1 Multiplica 10 por 5 y muestra el resultado mediante alert. 
+console.log(10 * 5);
 
-alert(10 / 2);
-console.log(alert)
+// 1.2 Divide 10 por 2 y muestra el resultado en un alert. 
+console.log(10 / 2)
+
 
 // 1.3 Muestra mediante un alert el resto de dividir 15 por 9. 
 
-alert(15 % 9);
-console.log(alert)
+console.log(15 % 9);
+
 
  // 1.4 Usa el correcto operador de asignación que resultará en x = 15, 
  // teniendo dos variables y = 10 y z = 5. 
--- upgrade-basic-algorithms/variables-avanzadas.js
+++ upgrade-basic-algorithms/variables-avanzadas.js
@@ -7,6 +7,7 @@
  let	firstName = 'Jon'; 
  let	lastName = 'Snow'; 
  let	age = 24; 
+ 
  // Muestralos por consola de esta forma: 
  // 	'Soy Jon Snow, tengo 24 años y me gustan los lobos.'
  console.log('Soy ' + firstName + ' ' + lastName + ', tengo ' + age + ' años y me gustan los lobos.');
