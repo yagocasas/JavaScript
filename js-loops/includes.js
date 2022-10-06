c2895de73019e6dc87e107dd9c7f0097f4f3122c
yagocasas <yagocd10@hotmail.com>
2022-10-07 00:28:36 +0200

    Iteration#1

---
+++ js-loops/includes.js
@@ -0,0 +1,15 @@
+// **Iteración #1: Usa includes**
+
+// Haz un bucle y muestra por consola todos aquellos valores del array que incluyan la palabra "Camiseta".
+//  Usa la función .***includes*** de javascript.
+
+const products = ['Camiseta de Pokemon', 'Pantalón coquinero', 
+'Gorra de gansta', 'Camiseta de Basket', 'Cinrurón de Orión', 'AC/DC Camiseta']
+
+products.forEach (function(elemento, index, array) {
+    if (elemento.includes('Camiseta')) {
+        console.log(elemento);
+    }
+} )
+
+ 
