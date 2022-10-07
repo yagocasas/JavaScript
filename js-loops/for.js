63280c839895c690af489424511441d53547b9ee
yagocasas <yagocd10@hotmail.com>
2022-10-07 12:18:27 +0200

    Iteration#5

---
+++ js-loops/for.js
@@ -0,0 +1,22 @@
+// **Iteración #5: Probando For**
+
+// Usa un bucle **for** para recorrer todos los destinos del array y elimina los elementos que tengan el id 11 y 40.
+// Imprime en un ***console log*** el array.
+// Puedes usar este array:
+
+const placesToTravel = [
+  { id: 5, name: "Japan" },
+  { id: 11, name: "Venecia" },
+  { id: 23, name: "Murcia" },
+  { id: 40, name: "Santander" },
+  { id: 44, name: "Filipinas" },
+  { id: 59, name: "Madagascar" },
+];
+
+for (let i = 0; i < placesToTravel.length; i++) {
+  const element = placesToTravel[i];
+    if(element['id'] === 11 || element['id'] === 40) { //utilizamos '.' para acceder a la propiedad y si utilizamos [''] (ojo!!!con comillas)
+        placesToTravel.splice(i, 1)
+    }
+}
+console.log(placesToTravel);
