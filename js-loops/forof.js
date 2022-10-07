5e4060b88ec4cd556f9ee729220946c79a71d447
yagocasas <yagocd10@hotmail.com>
2022-10-07 11:46:30 +0200

    Iteration#3

---
+++ js-loops/forof.js
@@ -0,0 +1,11 @@
+/* **Iteración #3: Probando For...of**
+
+Usa un bucle forof para recorrer todos los destinos del array. Imprime en un ***console.log*** sus valores.
+
+Puedes usar este array: */
+
+const placesToTravel = ['Japon', 'Venecia', 'Murcia', 'Santander', 'Filipinas', 'Madagascar']
+
+for (const place of placesToTravel) {
+    console.log(place)
+}
