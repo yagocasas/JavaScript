61f7c5970b27a854dea628b523e5ebf49d632f8f
yagocasas <yagocd10@hotmail.com>
2022-10-08 11:35:58 +0200

    Iteration#8 ADD comments

--- js-functions/functions8.js
+++ js-functions/functions8.js
@@ -17,16 +17,16 @@ const counterWords = [
   "code",
 ];
 function repeatCounter(array) {
-  let result = {};
+  let result = {}; //creamos una variable objeto vacío. Otros compañeros le han llamado counter. Perfecto!;
   for (let i = 0; i < array.length; i++) {
-    const element = array[i];
-    if (element in result) {  //tenemos un in
-      result[element]++;
+    const element = array[i]; //variable element a la que le asigo el valor de cada elemento del array en su posición;
+    if (element in result) {  //tenemos un ¡in!. Genial!!! (recuerda: element = array[i]);
+      result[element]++; //si la encuentra le suma 1. Es lo mismo que: result[element] = result[element] + 1;
     } else {
-      result[element] = 1;
+      result[element] = 1; //en caso contrario, el valor es 1 pq aparece una vez en el array y de aquí parten todos los items;
     }
   }
-  return result;
+  return result; //se retorna dentro de la función, fuera del bucle ;)
 }
 console.log(repeatCounter(counterWords));
 
